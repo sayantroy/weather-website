9762f7d91380d49a3973b3dcab7dda72c7f4eed4
Sayan <roys4356@gmail.com>
2022-02-20 23:21:30 +0530

    Setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ const m2=document.querySelector('#wet')
 weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    fetch(`/weather?address=${location}`).then((response)=>{
         response.json().then((data)=>{
             if(data.error)
                 m1.innerText=data.error
